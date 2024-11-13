4fb4494985a758cb00b477214f477e12c1408caa
Jessica Ni <jessicani@Jessicas-MBP.hsd1.va.comcast.net>
2024-11-12 21:28:09 -0500

    data project 1 get genre and get songs added

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import mysql.connector
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
@@ -28,4 +29,48 @@ def square(e: int):
 def subtract(f: int, g: int):
     return {"difference": f - g}
 
-# adding comments
+# ------- data project 1
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "dkh8my"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
+
+# ---------------
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        return None
+    cur.close()
+
+@app.get('/songs')
+def get_songs():
+    query = """SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image,
+           genres.genre 
+           FROM songs JOIN genres ON songs.genre = genres.genreid;"""
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        return None
+    cur.close()
+
